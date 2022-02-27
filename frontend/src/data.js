1d1b5dd61c6e426461cc57d3c98a55f0e0c73a63
Benematics <benjaminuwa60@gmail.com>
2022-02-27 20:08:01 +0100

    message

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+	products: [
+	{
+		_id:"1",
+		name: "Frontend Development",
+		category: "Web Development",
+		image: "/images/img.jpg",
+		type: "Development",
+		price: 80,
+		rating: 4.8,
+		description: "High quality",
+		numReviews: 10,
+	},
+		{
+		_id:"2",
+		name: "Backend Development",
+		category: "Web Development",
+		image: "/images/img.jpg",
+		type: "Development",
+		price: 80,
+		rating: 4.9,
+		description: "High quality",
+		numReviews: 12,
+	},
+		{
+		_id:"3",
+		name: "Mobile App Development",
+		category: "Mobile Development",
+		image: "/images/img.jpg",
+		type: "Development",
+		price: 100,
+		rating: 4.7,
+		description: "High quality",
+		numReviews: 10,
+	},
+		{
+		_id:"4",
+		name: "Data Science",
+		category: "Data Science",
+		image: "/images/img.jpg",
+		type: "Data",
+		price: 120,
+		rating: 4.6,
+		description: "High quality",
+		numReviews: 10,
+	},
+  ]
+}
+export default data;
